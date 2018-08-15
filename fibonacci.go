68768516c8197864822951b7033a31d04901e8fb
viewray <42269515+viewray@users.noreply.github.com>
2018-08-15 20:43:59 +0800

    Add files via upload

---
+++ fibonacci.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func(int) int {
+	return func(x int) int {
+		if x < 2 {
+			return x
+		}
+		a := fibonacci()
+		//不能用f
+		return (a(x-1) + a(x-2))
+	}
+
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Print(f(i), " ")
+	}
+}
