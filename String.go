8629c62d4c1cb5e413de61e25b5a343c7d347e88
viewray <42269515+viewray@users.noreply.github.com>
2018-08-21 22:35:14 +0800

    Update String.go

--- String.go
+++ String.go
@@ -44,3 +44,26 @@ func main() {
 	}
 
 }
+
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr.
+func (v IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v ", v[0], v[1], v[2], v[3])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	//fmt.Print(hosts["googleDNS"])
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+
+}
