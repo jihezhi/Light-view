2dfddf0d868086a3e5ebd19da41632cc4a96cdaf
viewray <42269515+viewray@users.noreply.github.com>
2018-12-17 13:32:18 +0800

    Update errex.go

--- errex.go
+++ errex.go
@@ -10,12 +10,12 @@ type ErrNegativeSqrt float64
 func (e ErrNegativeSqrt) Error() string {
 	return fmt.Sprint("cannot Sqrt negative number: ", float64(e))
 }
-
+/*
 func run(x float64) error {
 	return ErrNegativeSqrt(x)
 } //纱雾酱：这个run函数是多余的。考虑不创造这个run函数。
-
-func Sqrt(x float64) (float64, error) {
+*/
+func Sqrt1(x float64) (float64, error) {
 	z := 1.0
 	d := z
 	if x <= 0 {
@@ -34,4 +34,20 @@ func Sqrt(x float64) (float64, error) {
 func main() {
 	fmt.Println(Sqrt(2))
 	fmt.Println(Sqrt(-2))
+}
+
+func Sqrt(x float64) (float64, error) {
+	z := 1.0
+	d := z
+	if x <= 0 {
+		return x, ErrNegativeSqrt(x)
+	}
+	for i := 0; i < 10; i++ {
+		z =z- (z*z - x) / (2 * z)
+		if math.Abs(d-z) < 0.00000001 {
+			break
+		}
+
+	}
+	return z, nil
 }
