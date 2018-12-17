350520cf682d5cefb23302acd40b621fabf6da06
IzumiSagiri <0odevineo0@gmail.com>
2018-12-17 12:38:31 +0900

    Update mapsexercise.go

--- mapsexercise.go
+++ mapsexercise.go
@@ -9,7 +9,7 @@ func WordCount(s string) map[string]int {
 	sr := strings.Fields(s)
 	m := make(map[string]int)
 	for i := 0; i < len(sr); i++ {
-		if _, ok := m[sr[i]]；ok {
+		if _, ok := m[sr[i]]；ok { //纱雾酱：很棒，这才是优雅的Go写法。这样ok变量不会被泄露到main中，十分安全。
 			m[sr[i]] += 1
 		} else {
 			m[sr[i]] = 1
